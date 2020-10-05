f5ff3f469effd9666c0e2840973d0fa2d582485b
CHIkovil <chikovil3488@gmail.com>
2020-10-05 23:58:11 +0300

    userImageView added

--- SportQuest/ViewControllers/Authorization/RegistrationViewController.swift
+++ SportQuest/ViewControllers/Authorization/RegistrationViewController.swift
@@ -10,7 +10,28 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
     
+    //MARK: ImageView
+    lazy var userImageView: UIImageView = {
+        var imageView = UIImageView(image: UIImage(named: "camera.png"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = imageView.frame.size.width / 2
+        imageView.clipsToBounds = true
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderColor = UIColor.lightGray.cgColor
+        imageView.layer.borderWidth = 4
+        return imageView
+    }()
+    
+    //MARK: viewDidLoad
     override func viewDidLoad() {
         view.backgroundColor = .white
+        view.addSubview(userImageView)
+        createConstraintsUserImageView()
+    }
+    
+    //MARK: ConstraintsImageView
+    func createConstraintsUserImageView() {
+        userImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        userImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80).isActive = true
     }
 }
