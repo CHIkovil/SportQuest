791bb9a398c4951c895a149124cc1512949498e6
CHIkovil <chikovil3488@gmail.com>
2020-10-06 23:55:39 +0300

    nicknameRegistrationField and unicorn added

--- SportQuest/ViewControllers/Authorization/RegistrationViewController.swift
+++ SportQuest/ViewControllers/Authorization/RegistrationViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import TextFieldEffects
 
 class RegistrationViewController: UIViewController {
     
     //MARK: ImageView
     lazy var userImageView: UIImageView = {
-        var imageView = UIImageView(image: UIImage(named: "camera.png"))
+        var imageView = UIImageView(image: UIImage(named: "unicorn.png"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.layer.cornerRadius = imageView.frame.size.width / 2
         imageView.clipsToBounds = true
@@ -22,11 +23,33 @@ class RegistrationViewController: UIViewController {
         return imageView
     }()
     
+    //MARK: Field
+    lazy var nicknameRegistrationField: HoshiTextField = {
+        let textField = HoshiTextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = UIFont(name: "Chalkduster", size: 20)
+        textField.placeholder = "Nickname"
+        textField.placeholderColor = .lightGray
+        textField.placeholderFontScale = 0.8
+        textField.borderActiveColor = .black
+        textField.borderInactiveColor = .lightGray
+        textField.textColor = .lightGray
+        textField.tintColor = .lightGray
+        return textField
+    }()
+    
     //MARK: viewDidLoad
     override func viewDidLoad() {
         view.backgroundColor = .white
         view.addSubview(userImageView)
+        view.addSubview(nicknameRegistrationField)
         createConstraintsUserImageView()
+        createConstraintsNicknameRegistrationField()
+    }
+    
+    //MARK: touchesBegan
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
     
     //MARK: ConstraintsImageView
@@ -34,4 +57,12 @@ class RegistrationViewController: UIViewController {
         userImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         userImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80).isActive = true
     }
+    
+    func createConstraintsNicknameRegistrationField() {
+        nicknameRegistrationField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        nicknameRegistrationField.topAnchor.constraint(equalTo: userImageView.bottomAnchor, constant: 20).isActive = true
+        nicknameRegistrationField.widthAnchor.constraint(equalToConstant: 145).isActive = true
+        nicknameRegistrationField.heightAnchor.constraint(equalToConstant: 65).isActive =  true
+    }
+    
 }
